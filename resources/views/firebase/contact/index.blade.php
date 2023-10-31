bd8cc509f783c4be2e854ad32dd5767e4477bb86
Zainab <zainabsadat189@gmail.com>
2023-10-31 19:56:35 +0430

    new

--- resources/views/firebase/contact/index.blade.php
+++ resources/views/firebase/contact/index.blade.php
@@ -1,10 +1,8 @@
-@if(session('status'))
-<h4 class="alert">{{session('status')}}</h4>
-@endif
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 <div class="container">
-<a href="{{route('create')}}" class="btn btn-info my-5">add contact</a href="{{route('create')}}">
+<a href="{{route('create')}}" class="btn btn-info my-5">add contact</a>
 <table class="table table-striped">
 <thead>
     <tr>
